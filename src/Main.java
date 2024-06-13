d72f1d2e356df151977adde6939452fe6a10a800
POO <agusmaty23@gmail.com>
2024-06-13 09:23:31 -0500

    Nueva Version Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-import Reservas.Conferencia;
+import Reservas.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        Conferencia con1 =new Conferencia("francis",1000, 10202024);
+        //Conferencia con1 =new Conferencia("francis",1000, 10202024,"Conferencia",1000,);
+        Boda boda1 = new Boda("francis",1001,10062024,"Boda",2000);
+        boda1.CostoTotal();
+        boda1.calcularCostos();
+        Concierto con1 = new Concierto("francis",1000,12062024,"Concierto",5000);
+        con1.calcularCosto();
 
 
     }
