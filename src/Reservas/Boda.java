6b6d2eb3a6ce0d7e7c2741ed0244882f79e322d3
POO <agusmaty23@gmail.com>
2024-06-13 09:11:18 -0500

    Creacion Clase Boda

---
+++ src/Reservas/Boda.java
@@ -0,0 +1,21 @@
+package Reservas;
+
+public class Boda extends Evento {
+    public Boda(String cliente, int id, int fecha, String tipoEvento, double costoBase) {
+        super(cliente, id, fecha, tipoEvento, costoBase);
+    }
+
+    public void CostoTotal(){
+        System.out.println("Costo base de Boda: $2000");
+        System.out.println("Costo adicional por decoración floral: $500");
+        System.out.println("Costo adicional por música en vivo: $300");
+        System.out.println("Costo total de la boda: $2800");
+    }
+    int TotalCosto = 2800;
+    public void calcularCosto(){
+        super.calcularCostos();
+        System.out.println("Es un tipo de evento de caracarter: "+getTipoEvento());
+        System.out.println(" El costo base del evento es de: "+getCostoBase());
+        System.out.println(" El costo total del evento es de: $"+TotalCosto);
+    }
+}
